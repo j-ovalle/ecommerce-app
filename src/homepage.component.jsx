b413a5e97c5b721236656d4304ff9f9b00c5a70a
Juan Ovalle <juanovalle9@gmail.com>
2020-05-24 01:02:59 -0400

    Fix typo

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -7,35 +7,35 @@ const HomePage = () => (
             <div className="menu-item">
                 <div className='content'>
                     <h1 className='title'>HATS</h1>
-                    <h1 className='SUBtitle'>SHOP NOW</h1>
+                    <h1 className='Subtitle'>SHOP NOW</h1>
                     <h1 className='title'>HATS</h1>
                 </div>
             </div>
             <div className="menu-item">
                 <div className='content'>
                     <h1 className='title'>JACKETS</h1>
-                    <h1 className='SUBtitle'>SHOP NOW</h1>
+                    <h1 className='Subtitle'>SHOP NOW</h1>
                     <h1 className='title'>HATS</h1>
                 </div>
             </div>
             <div className="menu-item">
                 <div className='content'>
                     <h1 className='title'>SNEAKERS</h1>
-                    <h1 className='SUBtitle'>SHOP NOW</h1>
+                    <h1 className='Subtitle'>SHOP NOW</h1>
                     <h1 className='title'>HATS</h1>
                 </div>
             </div>
             <div className="menu-item">
                 <div className='content'>
                     <h1 className='title'>WOMENS</h1>
-                    <h1 className='SUBtitle'>SHOP NOW</h1>
+                    <h1 className='Subtitle'>SHOP NOW</h1>
                     <h1 className='title'>HATS</h1>
                 </div>
             </div>
             <div className="menu-item">
                 <div className='content'>
                     <h1 className='title'>MENS</h1>
-                    <h1 className='SUBtitle'>SHOP NOW</h1>
+                    <h1 className='Subtitle'>SHOP NOW</h1>
                     <h1 className='title'>HATS</h1>
                 </div>
             </div>
