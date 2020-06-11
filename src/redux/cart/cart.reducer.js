7e2feaeeb43952765f3a791ccdd1eb6a57c3fdd4
Juan Ovalle <juanovalle9@gmail.com>
2020-06-11 00:35:52 -0400

    Create add item redux action & reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,21 @@
-import { CartActionTypes } from './cart.types';
+import CartActionTypes from './cart.types';
 
-const INITIA_STATE = {
-    hidden: true
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
 };
 
-const cartReducer = (state = INITIA_STATE, action) => {
+const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: [...state.cartItems, action.payload]
             }
         default:
             return state;
