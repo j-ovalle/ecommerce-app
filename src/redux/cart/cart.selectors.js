018c651a87ed894d6bfe7929e9feee3feefb8752
Juan Ovalle <juanovalle9@gmail.com>
2020-06-12 00:10:56 -0400

    Update selectors with reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,7 +7,12 @@ export const selectCartItems = createSelector(
     (cart) => cart.cartItems
 );
 
-export const selectCartItemsCount =  createSelector(
+export const selectCartHidden = createSelector(
+    [selectCart],
+    (cart) => cart.hidden
+)
+
+export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
         cartItems.reduce((accumulatedQuantity, cartItem) => 
