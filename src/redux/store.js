2b65e5217470476528283ed06529f105a9b5558e
Juan Ovalle <juanovalle9@gmail.com>
2020-06-13 01:08:23 -0400

    Add persist modifications to cart storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { createStore} from 'redux';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
