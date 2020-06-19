cdfed74596c7ba04829a83bf5f1e9f3f8dee1cb2
Juan Ovalle <juanovalle9@gmail.com>
2020-06-19 00:02:37 -0400

    Made store.js code more legible

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-export const store = createStore(rootReducer, process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+    rootReducer, 
+    process.env.NODE_ENV === 'development' && 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const persistor = persistStore(store);
 
