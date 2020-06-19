e481a75fcafd864176c321e1711d44efb7d3cb72
Juan Ovalle <juanovalle9@gmail.com>
2020-06-18 23:57:59 -0400

    Modify redux logger condition

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() && process.env.NODE_ENV === 'development');
+export const store = createStore(rootReducer, process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export const persistor = persistStore(store);
 
