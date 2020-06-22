41a5f203f0f2dfccf3b4bf35755772ec97e8052f
Juan Ovalle <juanovalle9@gmail.com>
2020-06-22 00:48:26 -0400

    Move homepage styles to jsx and update component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+
+import { HomePageCointainer} from './homepage.styles';
 
 const HomePage = () => (
-    <div className='homepage'>
+    <HomePageCointainer>
         <Directory />
-    </div>
-)
+    </HomePageCointainer>
+);
 
 export default HomePage;
